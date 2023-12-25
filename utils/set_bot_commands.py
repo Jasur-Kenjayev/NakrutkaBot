bae8f7840fc12e9eac14d33e62e52a1ef4c39026
Jasur Kenjayev <94048770+Jasur-Kenjayev@users.noreply.github.com>
2023-12-25 12:19:29 +0500

    Add files via upload

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,10 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand("start", "♻️ Botni ishga tushurish"),
+            types.BotCommand("help", "🤖 bot haqida"),
+        ]
+    )
